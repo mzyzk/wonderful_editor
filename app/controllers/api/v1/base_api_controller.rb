f2da0a509621c6404e02f8a784b142e148528ce2
mzyzk <mzyzk85@outlook.com>
2025-07-16 23:47:31 +0900

    git commit -m "WIP: Task9-5 in-progress, failing specs"

--- app/controllers/api/v1/base_api_controller.rb
+++ app/controllers/api/v1/base_api_controller.rb
@@ -1,7 +1,6 @@
 # app/controllers/api/v1/base_api_controller.rb
 class Api::V1::BaseApiController < ApplicationController
-  # current_user のダミーコード
-  def current_user
-    @current_user ||= User.first
-  end
+  alias_method :current_user, :current_api_v1_user
+  alias_method :authenticate_user!, :authenticate_api_v1_user!
+  alias_method :user_signed_in?, :api_v1_user_signed_in?
 end
